b8bc110cf7068cbfa03c55feb3e0f9a31c9b3b9b
Adam Hallberg <adamdavidhallberg@gmail.com>
2023-02-19 12:19:55 +0100

    feat:player1Win func

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [win, setCount] = useState(0);
+const App = () => {
+  const [player1Points, setPlayer1Points] = useState(0);
+  const [player2Points, setPlayer2Points] = useState(0);
+  const [winner, setWinner] = useState('');
+
+  const player1Win = () => {
+    if (
+      (player1Points === 3 && player2Points < 3) ||
+      (player1Points === 4 && player2Points === 3)
+    ) {
+      setPlayer1Points(0);
+      setPlayer2Points(0);
+      setWinner('Player1 win');
+    } else if (player1Points === 3 && player2Points === 4) {
+      setPlayer2Points((prev) => prev--);
+    } else setPlayer1Points((prev) => prev++);
+  };
 
   return <div className="App"></div>;
-}
+};
 
 export default App;
